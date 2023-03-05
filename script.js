59e9b761290e6448df85808ad75eedeb2c185403
Cate <anncatherinedeguia@gmail.com>
2023-03-05 19:48:51 +0800

    Fix add book functionality

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const submitBtn = document.getElementById('submit-btn')
-const title = document.getElementById('title')
-const author = document.getElementById('author')
-const pages = document.getElementById('pages')
+const submitBtn = document.getElementById("submit-btn");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const error = document.querySelector('#error')
 
-submitBtn.addEventListener('click', (event) => {
-  event.preventDefault()
-  addBookToLibrary()
-  clear()
-  console.log(myLibrary)
-  display()
-})
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  clear();
+  // closeForm()
+});
 
 let myLibrary = [
+  {title: 'Everyday', author: 'David Levithan', pages: '394'},
 ];
 
 function openForm() {
@@ -26,22 +27,29 @@ function Book() {
   // the constructor...
 }
 
-function addBookToLibrary(form) {
+function addBookToLibrary() {
+
+  if(title.value === '' || author.value === '' || pages.value === '') {
+    error.innerHTML = 'pls fill up form'
+  }else{
+    myLibrary.push({
+    title: title.value,
+    author: author.value,
+    pages: pages.value,
+  });
+  display()
+
+  }
+  
 
 
-        myLibrary.push(
-            {
-                title: title.value,
-                author: author.value,
-                pages: pages.value,
-            },
-        );
+  
 }
 
 function clear() {
-  title.value = ''
-  author.value = ''
-  pages.value = ''
+  title.value = "";
+  author.value = "";
+  pages.value = "";
 }
 
 function display() {
@@ -50,19 +58,24 @@ function display() {
   const title = document.createElement("h3");
   const author = document.createElement("p");
   const pages = document.createElement("p");
+  const delBtn = document.createElement('button')
+  delBtn.classList = ' del-btn'
+
 
   myLibrary.forEach((book) => {
-  
-  card.classList = "card-body";
+    card.classList = "card-body";
 
-  title.innerHTML = book.title;
-  author.innerHTML = book.author;
-  pages.innerHTML = book.pages;
+    title.innerHTML = book.title;
+    author.innerHTML = book.author;
+    pages.innerHTML = book.pages;
+    delBtn.innerHTML = 'Remove'
 
-  card.append(title);
-  card.append(author);
-  card.append(pages);
-});
-container.append(card);
+    card.append(title);
+    card.append(author);
+    card.append(pages);
+    card.append(delBtn)
+  });
+  container.append(card);
 }
 
+display()
