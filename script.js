80c9eceb48322c4e5b5f871a447698c22e61c245
Ann Catherine <catherine.acee@gmail.com>
2023-03-06 19:06:53 +0800

    Try to save my code failed. i hate backend :D

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const submitBtn = document.getElementById("submit-btn");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const error = document.querySelector('#error')
+// const title = document.getElementById("title");
+// const author = document.getElementById("author");
+// const pages = document.getElementById("pages");
+const error = document.querySelector("#error");
+
+var title = document.querySelector("#title");
+var author = document.querySelector("#author");
+var pages = document.querySelector("#pages");
+var resultsDiv = document.querySelector("#results");
 
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
   addBookToLibrary();
   clear();
   // closeForm()
+  render();
 });
 
-let myLibrary = [
-  {title: 'Everyday', author: 'David Levithan', pages: '394'},
-];
+
+
+let myLibrary = [{ title: "Everyday", author: "David Levithan", pages: "394" }];
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
@@ -28,22 +34,16 @@ function Book() {
 }
 
 function addBookToLibrary() {
-
-  if(title.value === '' || author.value === '' || pages.value === '') {
-    error.innerHTML = 'pls fill up form'
-  }else{
+  if (title.value === "" || author.value === "" || pages.value === "") {
+    error.innerHTML = "pls fill up form";
+  } else {
     myLibrary.push({
-    title: title.value,
-    author: author.value,
-    pages: pages.value,
-  });
-  display()
-
+      title: title.value,
+      author: author.value,
+      pages: pages.value,
+    });
+    display();
   }
-  
-
-
-  
 }
 
 function clear() {
@@ -58,24 +58,41 @@ function display() {
   const title = document.createElement("h3");
   const author = document.createElement("p");
   const pages = document.createElement("p");
-  const delBtn = document.createElement('button')
-  delBtn.classList = ' del-btn'
+  const delBtn = document.createElement("button");
+  delBtn.classList = " del-btn";
+card.classList = "card-body";
+
+
 
 
   myLibrary.forEach((book) => {
-    card.classList = "card-body";
-
+    
     title.innerHTML = book.title;
     author.innerHTML = book.author;
     pages.innerHTML = book.pages;
-    delBtn.innerHTML = 'Remove'
+    delBtn.innerHTML = "Remove";
 
     card.append(title);
     card.append(author);
     card.append(pages);
-    card.append(delBtn)
-  });
-  container.append(card);
+    card.append(delBtn);
+ 
+  });   
+  container.append(card)
+  delBtn.setAttribute('onclick','deleteBook(book.title)')
 }
 
-display()
+display();
+
+// function render(){
+//   resultsDiv.innerHTML="";
+//   for(object of myLibrary)
+//    resultsDiv.innerHTML+= `Title:${object.title}  Author:${object.author} pages:${object.pages} <button onclick="deleteBook('${object.title}')">Delete</button><br>`;            
+
+// }
+
+function deleteBook(title){
+  let index=myLibrary.findIndex( book => book.title == title);
+  myLibrary.splice(index,1);
+  display();
+}
